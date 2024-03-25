17886ccbee8f23d9c9e15b875adecadbb7ade6df
ForegroundMattia <mmarranzino23@fed.idserve.net>
2024-03-25 12:49:26 +0100

    First Push

---
+++ UIKitPorfolio/UIKitPorfolio/Data/DataFilm.swift
@@ -0,0 +1,19 @@
+//
+//  DataFilm.swift
+//  UIKitPorfolio
+//
+//  Created by Mattia Marranzino on 25/03/24.
+//
+
+import Foundation
+import SwiftUI
+
+var films : [FilmPoster] = [
+
+
+
+
+]
+
+
+
---
+++ UIKitPorfolio/UIKitPorfolio/Data/FilmPoster.swift
@@ -0,0 +1,20 @@
+//
+//  FilmPoster.swift
+//  UIKitPorfolio
+//
+//  Created by Mattia Marranzino on 25/03/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct FilmPoster{
+    
+    var titolo : String
+    var rate : Int
+    var comment : String
+    var poster : Image
+    
+    
+}
